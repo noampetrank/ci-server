e27eaba3439656c2b449f071e43d2833c656a2d6
Yotam Karmon <yotam@bugatone.com>
2018-07-24 14:44:57 +0300

    Add middleware for Verifying Github signature

--- routes/github.js
+++ routes/github.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var verifyGithubWebhook = require("verify-github-webhook").default;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(200);
+GITHUB_APP_SECRET = "6f1cc7d2c60e3c5a0cffb24ecb8c8594fc46ca23";
+
+function verifySignature(signature, payload) {
+  return verifyGithubWebhook(signature, payload, GITHUB_APP_SECRET);
+}
+
+router.use(async(req, res, next) => {
+  console.log(req.body);
+  console.log(req.headers["x-hub-signature"]);
+
+  validSignature = await verifySignature(req.headers["x-hub-signature"], JSON.stringify(req.body));
+  if (validSignature) {
+    next();
+  }
+  else {
+    res.status(401).send({ error: 'Invalid signature' })
+  }
+})
+
+router.post('/', async (req, res) => {
+  console.log("success");
+  res.sendStatus(200);
 });
 
 module.exports = router;
