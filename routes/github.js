3fb30076868d0f8a562f397447b7738ac5105e19
Yotam Karmon <yotam@bugatone.com>
2018-07-24 14:56:07 +0300

    Refactor Github route to be more readable

--- routes/github.js
+++ routes/github.js
@@ -11,7 +11,6 @@ const LogService = require("../services/log");
 const SystemService = require("../services/system");
 
 const GITHUB_REPO_OWNER = "bugatone";
-const GITHUB_REPO_NAME = "mobileproduct";
 
 const LOG_PATH = process.env.BUGATONE_CI_ROOT + "/public";
 const LOG_FILE_NAME_PREFIX = "test_output";
@@ -44,38 +43,60 @@ function saveLog(commitId, log) {
     return SystemService.writeFile(logPath, log);
 }
 
+function createResultMessage(testsPassed, totalTestTime, totalQueueAndTestTime, commitId, testOutput, isShortMessage) {
+    let message;
+    if (testsPassed) {
+        message = "Tests passed.\n";
+    }
+    else {
+        message = "Tests failed.\n";
+    }
+    message += getTimingReport(totalTestTime, totalQueueAndTestTime);
+    if (isShortMessage) {
+        return message;
+    }
+
+    message += "\nSee full log at: " + getLogAddress(commitId);
+
+    if (!testsPassed && testOutput) {
+        message += "\n```\n" + testOutput.slice(-MAX_LOG_LENGTH_IN_GITHUB_COMMENTS) + "\n```";
+    }
+    return message;
+}
+
+function getCommitState(testsPassed) {
+    if (testsPassed) {
+        return "success";
+    }
+    else {
+        return "failure";
+    }
+}
+
+function getTimingReport(totalTestTime, totalQueueAndTestTime) {
+    return "Testing took " + totalTestTime + "s. Total time: " + totalQueueAndTestTime + "s (including queue).";
+}
+
 function handleTestResult(repoName, pullRequestNum, commitId, testsPassed, testOutput, totalTestTime, totalQueueAndTestTime) {
     return new Promise(async (resolve, reject) => {
-        let timingReport = "Testing took " + totalTestTime + "s. Total time: " + totalQueueAndTestTime + "s (including queue).";
-        LogService.log("Timing of " + commitId + ": " + timingReport);
+        let isPullRequest = !!pullRequestNum;
+        
+        LogService.log("Timing of " + commitId + ": " + getTimingReport(totalTestTime, totalQueueAndTestTime));
 
         try {
-            let shortMessage;
-            let status;
-            if (testsPassed) {
-                shortMessage = "Tests passed.\n";
-                status = "success";
-            }
-            else {
-                shortMessage = "Tests failed.\n";
-                status = "failure";
-            }
-            shortMessage += timingReport;
-            let fullMessage = shortMessage + "\nSee full log at: " + getLogAddress(commitId);
+            let shortMessage = createResultMessage(testsPassed, totalTestTime, totalQueueAndTestTime, commitId, testOutput, true);
+            let fullMessage = createResultMessage(testsPassed, totalTestTime, totalQueueAndTestTime, commitId, testOutput, false);
 
-            if (!testsPassed && testOutput) {
-                fullMessage += "\n```\n" + testOutput.slice(-MAX_LOG_LENGTH_IN_GITHUB_COMMENTS) + "\n```";
-            }
-
-            await GithubService.setCommitStatus(GITHUB_REPO_OWNER, repoName, commitId, status, shortMessage);
+            await GithubService.setCommitStatus(GITHUB_REPO_OWNER, repoName, commitId, getCommitState(testsPassed), shortMessage);
 
             await saveLog(commitId, testOutput);
             LogService.log("log saved successfully");
 
-            if (pullRequestNum) {
+            if (isPullRequest) {
                 await GithubService.postPullRequestComment(GITHUB_REPO_OWNER, repoName, pullRequestNum, fullMessage);
             }
             else {
+                // This is a push to master.
                 await GithubService.postCommitComment(GITHUB_REPO_OWNER, repoName, commitId, fullMessage);
                 if (!testsPassed) {
                     await EmailService.sendEmail(process.env.BUGATONE_NOTIFICATION_EMAILS, "Tests failed in master for commit " + commitId, fullMessage);
@@ -111,60 +132,75 @@ async function notifyTestError(repoName, commitId, errorMessage, testOutput) {
     }
 }
 
+function getEventAction(requestBody) {
+    let action = requestBody.action;
+    if (!action) {
+        if (requestBody.ref == "refs/heads/master") {
+            action = "push_to_master";
+        }
+        else {
+            return null;
+        }
+    }
+    return action;
+}
+
+function getEventParams(requestBody) {
+    let action = getEventAction(requestBody)
+
+    switch(action) {
+        case "opened":
+        case "synchronize":
+            LogService.log("Commit pushed into pull request #" + requestBody.number);
+            return {
+                commitId: requestBody.pull_request.head.sha,
+                branch: requestBody.pull_request.head.ref,
+                pullRequestNum: requestBody.number,
+                repoName: requestBody.repository.name
+            };
+        case "push_to_master":
+            LogService.log("Commit pushed into master");
+            return {
+                commitId: requestBody.after,
+                branch: "master",
+                repoName: requestBody.repository.name
+            };
+        default:
+            if (action) {
+                LogService.log("Unhandled action: " + action);
+            }
+            else {
+                LogService.log("Unrecognized action");
+            }
+            return null;
+    }
+}
+
 router.post('/', async (req, res) => {
-    let commitId;
-    let pullRequestNum;
-    let branch;
-    let repoName = GITHUB_REPO_NAME;
     let testQueued = false;
 
     try {
-        // First we send a response. Then we handle the action.
+        // First we send a response. Then we handle the event.
         res.sendStatus(200);
 
-        let action = req.body.action;
-        if (!action) {
-            if (req.body.ref == "refs/heads/master") {
-                action = "push_to_master";
-            }
-            else {
-                LogService.log("Ignoring request");
-                return;
-            }
+        let eventParams = getEventParams(req.body);
+        if (!eventParams) {
+            LogService.log("Ignoring request");
+            return;
         }
 
-        LogService.log("action = " + action)
-        switch(action) {
-            case "opened":
-            case "synchronize":
-                commitId = req.body.pull_request.head.sha;
-                branch = req.body.pull_request.head.ref;
-                pullRequestNum = req.body.number;
-
-                break;
-            case "push_to_master":
-                commitId = req.body.after;
-                branch = "master";
-                repoName = req.body.repository.name;
-
-                break;
-            default:
-                LogService.log("Unhandled action: " + action);
-                return;
-        }
-
+        await notifyTestInProgress(eventParams.repoName, eventParams.commitId);
         testQueued = true;
-        await notifyTestInProgress(repoName, commitId);
         let queueStartTime = new Date();
         
-        let { testsPassed, testOutput, totalTestTime } = await TestService.runTests(commitId, branch);
+        let { testsPassed, testOutput, totalTestTime } = await TestService.runTests(eventParams.commitId, eventParams.branch);
         
         let totalQueueAndTestTime = (new Date() - queueStartTime) / 1000;
-        await handleTestResult(repoName, pullRequestNum, commitId, testsPassed, testOutput, totalTestTime, totalQueueAndTestTime);
+        await handleTestResult(eventParams.repoName, eventParams.pullRequestNum, eventParams.commitId, testsPassed, testOutput, totalTestTime, totalQueueAndTestTime);
     } catch(err) {
-        LogService.error("Unexpected exception (commit " + commitId + "): " + JSON.stringify(err));
+        LogService.error("Unexpected exception (commit " + eventParams.commitId + "): " + JSON.stringify(err));
         if (testQueued) {
-            notifyTestError(repoName, commitId, err.message, err.output);
+            notifyTestError(eventParams.repoName, eventParams.commitId, err.message, err.output);
         }
     }
 });
